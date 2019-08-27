a6006fc1ddf1fa8958214fd71531574931a18e96
GinoC <ginoachacon@gmail.com>
2019-08-27 16:21:35 -0500

    Not needed

--- run_champ_trace.sh
+++
@@ -1,3 +0,0 @@
-./run_champsim.sh perceptron-no-no-no-lru-1core 0 500 605.mcf_s-665B.champsimtrace.xz; mv trace.out mcf500M.trace;
-./run_champsim.sh perceptron-no-no-no-lru-1core 0 500 607.cactuBSSN_s-2421B.champsimtrace.xz; mv trace.out cactus500M.trace;
-./run_champsim.sh perceptron-no-no-no-lru-1core 0 500 619.lbm_s-4268B.champsimtrace.xz; mv trace.out lbm500M.trace;
