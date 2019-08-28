e232284c026df8727281a367958a599ae0238a0c
GinoC <ginoachacon@gmail.com>
2019-08-28 15:14:18 -0500

    Extra script

--- 1B_test_run.sh
+++
@@ -1,7 +0,0 @@
-./run_champsim.sh perceptron-next_line-kpcp-no-lru-1core 200 1000 600.perlbench_s-210B.champsimtrace.xz &
-./run_champsim.sh perceptron-no-kpcp-no-lru-1core 200 1000 600.perlbench_s-210B.champsimtrace.xz &
-./run_champsim.sh perceptron-no-no-no-lru-1core 200 1000 600.perlbench_s-210B.champsimtrace.xz &
-./run_champsim.sh perceptron-next_line-no-no-lru-1core 200 1000 600.perlbench_s-210B.champsimtrace.xz &
-./run_champsim.sh perceptron-next_line_throttle-no-no-lru-1core 200 1000 600.perlbench_s-210B.champsimtrace.xz &
-./run_champsim.sh perceptron-next_line_throttle-kpcp-no-lru-1core 200 1000 600.perlbench_s-210B.champsimtrace.xz &
-
